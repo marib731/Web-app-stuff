387d9f9ee4c531e1c4009ecb57cfd39081c40348
Jessica Butler <jessie.butler89@gmail.com>
2019-04-05 13:51:26 +0100

    fixed git problems

--- webapp/webscrappingscriptbikes.py
+++ webapp/webscrappingscriptbikes.py
@@ -5,10 +5,11 @@ Created on Tue Feb 26 14:51:29 2019
 @author: Jessie
 """
 
-import mysql.connector
 import requests
 import time
-import traceback
+import sqlalchemy
+
+
 def fix_data(row):
     row['StationID'] = row['number']
     del row['number']
@@ -18,93 +19,31 @@ def fix_data(row):
     return row
  
  
-def get_conn():
+def get_engine():
     try:
-        conn = mysql.connector.connect(user="EnxiJessieMarian",
-                                   passwd="SoftwareEngineering2019",
-                                   host="dublinbikesdata.cmgmbuuwvwd0.eu-west-1.rds.amazonaws.com",
-                                   database='DublinBikesData')
-        return conn
+        engine = sqlalchemy.create_engine('mysql+pymysql://EnxiJessieMarian:SoftwareEngineering2019@dublinbikesdata.cmgmbuuwvwd0.eu-west-1.rds.amazonaws.com:3306/DublinBikesData', echo=True)
+        return engine
     except:
-        print("Connection to RDS instance failed", traceback.format_exc())
+        print("Connection to RDS instance failed")
         return None
      
-def main():
-    conn = get_conn()
-    mycursor = conn.cursor()      
+def main():  
+    engine = get_engine()
+    conn = engine.connect()  
     while True:
         url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=166048e0e00bbc76dd9a53d07bab98427b29d1e0"
         the_page = requests.get(url).json()
-        #print(the_page) #prints the JSON file retrieved with API
-        #print("Data returned")
         for row in the_page:
-            table_name = "DublinBikesData"
             if 'number' in row:
                 row = fix_data(row)
-            #print(row)
-            placeholder = ", ".join(["%s"] * len(row))
-            
-            
-            #print(placeholder)
-            sql = "insert ignore into {table} ({columns}) values ({values});".format(table=table_name, columns=",".join(row.keys()), 
-                                                                                   values=placeholder)
-            #print(sql)
-            mycursor.execute(sql, list(row.values()))
-            conn.commit()
+            sql = "insert ignore into {table} ({columns}) values ({values});".format(table="DublinBikesData", columns=",".join(row.keys()), 
+                                                                                   values=", ".join(["%s"] * len(row)))
+            #sql = DublinBikesData.insert().\values(name=bindparam({columns}).format(columns=",".join(row.keys())
+            conn.execute(sql, list(row.values()))
+        
         print("Sleeping")
-        time.sleep(2)
-     
-    conn.commit()
-    conn.close()
+        time.sleep(1000)
+    engine.close()
      
 if __name__ == '__main__':
     main()
-
-
-# import mysql.connector
-# import requests
-# #import pandas as pd
-# #from bs4 import BeautifulSoup
-# #import json
-# import time
-# 
-# def fix_data(row):
-#     row['StationID'] = row['number']
-#     del row['number']
-#     row['position_lat'] = row['position']['lat']
-#     row['position_lng'] = row['position']['lng']
-#     del row['position']
-#     return row
-# 
-# 
-# 
-# try:
-#     conn = mysql.connector.connect(user="EnxiJessieMarian",
-#                                    passwd="SoftwareEngineering2019",
-#                                    host="dublinbikesdata.cmgmbuuwvwd0.eu-west-1.rds.amazonaws.com",
-#                                    database='DublinBikesData')
-#     mycursor = conn.cursor()
-# 
-# except:
-#     print("Connection to RDS instance failed")
-# try:
-#     while True:
-#     
-#         url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=166048e0e00bbc76dd9a53d07bab98427b29d1e0"
-#         the_page = requests.get(url).json()
-#         print(the_page) #prints the JSON file retrieved with API
-#         print("Data returned")
-#         for row in the_page:
-#             table_name = "DublinBikesData"
-#             if 'number' in row:
-#                 row = fix_data(row)
-#             placeholder = ", ".join(["%s"] * len(row))
-#             sql = "insert ignore into {table} ({columns}) values ({values});".format(table=table_name, columns=",".join(row.keys()), 
-#                                                                                        values=placeholder)
-#             res = mycursor.execute(sql, list(row.values()))
-#         conn.commit()
-#         print("Sleeping")
-#         time.sleep(2)
-# except:
-#     print("Error accessing JCDecaux")
-# conn.close()
