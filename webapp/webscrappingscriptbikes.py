92056e67944d8751c8ea5977183d1fb9fe9f24f1
Jessica Butler <jessie.butler89@gmail.com>
2019-04-05 14:11:04 +0100

    removed conflict markers

--- webapp/webscrappingscriptbikes.py
+++ webapp/webscrappingscriptbikes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 26 14:51:29 2019
@@ -47,67 +46,4 @@ def main():
     engine.close()
      
 if __name__ == '__main__':
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 26 14:51:29 2019
-
-@author: Jessie
-"""
-
-import mysql.connector
-import requests
-import time
-
-def fix_data(row):
-    row['StationID'] = row['number']
-    del row['number']
-    row['position_lat'] = row['position']['lat']
-    row['position_lng'] = row['position']['lng']
-    del row['position']
-    return row
- 
- 
-def get_conn():
-    try:
-        conn = mysql.connector.connect(user="EnxiJessieMarian",
-                                   passwd="SoftwareEngineering2019",
-                                   host="dublinbikesdata.cmgmbuuwvwd0.eu-west-1.rds.amazonaws.com",
-                                   database='DublinBikesData')
-        #print(conn)
-        return conn
-    except:
-        print("Connection to RDS instance failed")
-        return None
-     
-def main():
-    conn = get_conn()
-    mycursor = conn.cursor()      
-    while True:
-        url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=166048e0e00bbc76dd9a53d07bab98427b29d1e0"
-        the_page = requests.get(url).json()
-        #print(the_page) #prints the JSON file retrieved with API
-        #print("Data returned")
-        for row in the_page:
-            table_name = "DublinBikesData"
-            if 'number' in row:
-                row = fix_data(row)
-            #print(row)
-            placeholder = ", ".join(["%s"] * len(row))
-            
-            
-            #print(placeholder)
-            sql = "insert ignore into {table} ({columns}) values ({values});".format(table=table_name, columns=",".join(row.keys()), 
-                                                                                   values=placeholder)
-            #print(sql)
-            mycursor.execute(sql, list(row.values()))
-            conn.commit()
-        print("Sleeping")
-        time.sleep(600)
-     
-    conn.commit()
-    conn.close()
-     
-if __name__ == '__main__':
->>>>>>> d0ccea6d9880ab50f7306ef4b626fc5627efd274
     main()
